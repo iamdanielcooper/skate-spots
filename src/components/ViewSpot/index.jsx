bc248c84439010f230d77125a8abf7125797cb39
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-10-05 22:03:29 +0100

    update spot to play video not image

--- src/components/ViewSpot/index.jsx
+++ src/components/ViewSpot/index.jsx
@@ -2,22 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import styles from './style.module.css';
 
 const ViewSpot = ({ spotData }) => {
-    const [image, setImage] = useState('');
     const hitSpotSection = useRef(null);
-
-    useEffect(() => {
-        var reader = new FileReader();
-
-        reader.onloadend = function () {
-            setImage(reader.result);
-        };
-
-        if (spotData.image) {
-            reader.readAsDataURL(spotData.image);
-        } else {
-            setImage('');
-        }
-    }, [spotData.image]);
 
     const displaySpotDate = date => {
         return date.split(', ')[0];
@@ -25,10 +10,17 @@ const ViewSpot = ({ spotData }) => {
 
     return (
         <div className={styles.container}>
-            {/* <img className={styles.spotImage} alt='The spot' src={image} /> */}
-            <video width='320' height='240' controls>
-                <source src='movie.mp4' type='video/mp4' />
-                <source src='movie.ogg' type='video/ogg' />
+            <video
+                className={styles.spotVideo}
+                autoPlay
+                loop
+                webkit-playsinline
+                playsinline
+            >
+                <source
+                    src={URL.createObjectURL(spotData.image)}
+                    type='video/mp4'
+                />
                 Your browser does not support the video tag.
             </video>
             <h1 className={styles.spotTitle}>{spotData.name}</h1>
